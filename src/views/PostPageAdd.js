51385fe56cd4dfbbeddeb13a7f60bc04c7d89f69
wheyy <78036862+wheyy@users.noreply.github.com>
2024-08-06 10:57:57 +0800

    UserAuth for addpostpage

---
+++ src/views/PostPageAdd.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
+import { addDoc, collection } from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom" ;
+import { auth, db } from "../firebase";
+import { signOut } from "firebase/auth";
+
+
+
+export default function PostPageAdd() {
+  const [user,loading] = useAuthState(auth);
+  const [caption, setCaption] = useState("");
+  const [image, setImage] = useState("");
+  const navigate = useNavigate();
+
+  async function addPost() {
+    await addDoc(collection(db,"posts"), {caption, image});
+    navigate("/");
+  }
+
+  useEffect(() => {
+    if (loading) return;
+    if (!user) return navigate("/login");
+  }, [navigate, user, loading]);
+
+  return (
+    <>
+      <Navbar variant="light" bg="light">
+        <Container>
+          <Navbar.Brand href="/">Tinkergram</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="/add">New Post</Nav.Link>
+            <Nav.Link onClick={(e) => signOut(auth)}>Sign Out</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Container>
+        <h1 style={{ marginBlock: "1rem" }}>Add Post</h1>
+        <Form>
+          <Form.Group className="mb-3" controlId="caption">
+            <Form.Label>Caption</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Lovely day"
+              value={caption}
+              onChange={(text) => setCaption(text.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="image">
+            <Form.Label>Image URL</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="https://zca.sg/img/1"
+              value={image}
+              onChange={(text) => setImage(text.target.value)}
+            />
+            <Form.Text className="text-muted">
+              Make sure the url has a image type at the end: jpg, jpeg, png.
+            </Form.Text>
+          </Form.Group>
+          <Button variant="primary" onClick={async (e) => addPost()}>
+            Submit
+          </Button>
+        </Form>
+      </Container>
+    </>
+  );
+}
